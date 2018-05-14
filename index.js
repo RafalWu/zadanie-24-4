8af4b9aebddd9d7527a483f32ba8a9f6247d9e6f
Rafal Wrobel <wrobelrafal1992@gmail.com>
2018-05-14 11:41:24 +0200

    Change format

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const formatDate = (timeInSeconds) => {
-  return `${timeInSeconds}s`;
+
+function formatDate(seconds) {
+  const h = Math.floor(seconds / 3600);
+  const m = Math.floor((seconds % 3600) / 60);
+  const s = seconds % 60;
+
+  return [
+    h !== 0 ? h + 'h' : '',
+    m !== 0 ? m + 'm' : '',
+    s !== 0 ? s + 's' : ''
+  ].join(' ').trim();
 }
 
 module.exports = formatDate;
