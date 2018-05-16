4314072b9bbd5a7e79c2d50571870d75f68f074f
Rafal Wrobel <wrobelrafal1992@gmail.com>
2018-05-16 15:02:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-
 function formatDate(seconds) {
   const h = Math.floor(seconds / 3600);
   const m = Math.floor((seconds % 3600) / 60);
   const s = seconds % 60;
 
-  return [
-    h !== 0 ? h + 'h' : '',
-    m !== 0 ? m + 'm' : '',
-    s !== 0 ? s + 's' : ''
-  ].join(' ').trim();
+  const result = [];
+
+	if (h !== 0) {
+		result.push(h + 'h');
+	}
+
+	if (m !== 0) {
+		result.push(m + 'm');
+	}
+
+	if (s !== 0) {
+		result.push(s + 's');
+	}
+	
+	if(isNaN(seconds)) return "0s"; 
+		else {
+		  return result.join(' ');
+		}
 }
 
+	
 module.exports = formatDate;
